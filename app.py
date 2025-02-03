6d7c827d8bc258765c3d6cf73f60523b64d5f60e
Larissa Cassanjo <larissacassanjo@gmail.com>
2025-02-03 10:42:01 -0300

    eventos e banco de dados

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ def main(page: ft.Page): # Atribui uma página em branco do flet a uma função
     
     page.title = 'Cadastro App'
     
+    def cadastrar(e):
+        print(produto.value)
+        print(preco.value)
+    
     txt_titulo = ft.Text('Título do produto: ')
     produto = ft.TextField(label='Digite o título do produto',text_align=ft.TextAlign.LEFT)
     txt_preco = ft.Text('Preço do produto')
     preco = ft.TextField(value=0,label='Digite o preço do produto',text_align=ft.TextAlign.LEFT)
-    btn_produto = ft.ElevatedButton('Cadastrar')
+    btn_produto = ft.ElevatedButton('Cadastrar', on_click=cadastrar)
     
     page.add(
         txt_titulo,
