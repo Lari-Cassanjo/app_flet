b43c37e2b4aefaab59554e7c8f338af470dd8cc6
Larissa Cassanjo <larissacassanjo@gmail.com>
2025-02-03 11:05:43 -0300

    mensagens de sucesso e erro

--- app.py
+++ app.py
@@ -16,12 +16,30 @@ def main(page: ft.Page): # Atribui uma página em branco do flet a uma função
     lista_produtos = ft.ListView()
     
     def cadastrar(e):
-        novo_produto = Produto(titulo=produto.value, preco=preco.value)
-        session.add(novo_produto)
-        session.commit()
-        lista_produtos.controls.append(ft.Text(produto.value))
+        try:
+            novo_produto = Produto(titulo=produto.value, preco=preco.value)
+            session.add(novo_produto)
+            session.commit()
+            lista_produtos.controls.append(
+                ft.Container(
+                    ft.Text(produto.value),
+                    bgcolor=ft.colors.BLACK,
+                    padding=15,
+                    alignment=ft.alignment.center,
+                    margin=3,
+                    border_radius=10
+                )
+            )
+            txt_erro.visible = False
+            txt_acerto.visible = True
+        except:
+            txt_erro.visible = True
+            txt_acerto.visible = False
         page.update()
         print('Produto salvo com sucesso!')
+        
+    txt_erro = ft.Container(ft.Text('Erro ao salvar o produto!'), visible=False, bgcolor=ft.colors.RED, padding=10, alignment=ft.alignment.center)
+    txt_acerto = ft.Container(ft.Text('Produto salvo com sucesso!'), visible=False, bgcolor=ft.colors.GREEN, padding=10, alignment=ft.alignment.center)
     
     txt_titulo = ft.Text('Título do produto: ')
     produto = ft.TextField(label='Digite o título do produto',text_align=ft.TextAlign.LEFT)
@@ -34,13 +52,20 @@ def main(page: ft.Page): # Atribui uma página em branco do flet a uma função
         produto,
         txt_preco,
         preco,
-        btn_produto
+        btn_produto,
+        txt_acerto,
+        txt_erro
     )
     
     for p in session.query(Produto).all():
         lista_produtos.controls.append(
             ft.Container(
-                ft.Text(p.titulo)
+                ft.Text(p.titulo),
+                bgcolor=ft.colors.BLACK,
+                padding=15,
+                alignment=ft.alignment.center,
+                margin=3,
+                border_radius=10
             )
         )
         
